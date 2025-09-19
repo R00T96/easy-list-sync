e858fd781ad2871d01b2aab972c7b53f92207cea
romesh <romesh.niriella@btcmarkets.net>
2025-09-19 17:23:31 +1000

    event emitter skeleton

---
+++ src/events/EventRegistry.ts
@@ -0,0 +1,24 @@
+// Registry for managing multiple event emitters (modular, DRY)
+import type { IEventEmitter } from "./IEventEmitter";
+import type { AppEvent } from "./eventTypes";
+
+class EventRegistry {
+  private emitters: IEventEmitter[] = [];
+
+  register(emitter: IEventEmitter) {
+    this.emitters.push(emitter);
+  }
+
+  unregister(emitter: IEventEmitter) {
+    this.emitters = this.emitters.filter(e => e !== emitter);
+  }
+
+  emit(event: AppEvent) {
+    for (const emitter of this.emitters) {
+      emitter.emit(event);
+    }
+  }
+}
+
+// Singleton instance for app-wide use
+export const eventRegistry = new EventRegistry();
